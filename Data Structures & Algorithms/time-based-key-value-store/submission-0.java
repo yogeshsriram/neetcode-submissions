f720828e5cbecdc5ddbcbe7bfc004881a3427aaa
Pinisetti Yogesh Sri Ram <yogeshsriram13@gmail.com>
2026-08-24 14:30:39 +0530

    Add: time-based-key-value-store - submission-0

---
+++ Data Structures & Algorithms/time-based-key-value-store/submission-0.java
@@ -0,0 +1,33 @@
+public class TimeMap {
+    private Map<String, Map<Integer, List<String>>> keyStore;
+
+    public TimeMap() {
+        keyStore = new HashMap<>();
+    }
+
+    public void set(String key, String value, int timestamp) {
+        if (!keyStore.containsKey(key)) {
+            keyStore.put(key, new HashMap<>());
+        }
+        if (!keyStore.get(key).containsKey(timestamp)) {
+            keyStore.get(key).put(timestamp, new ArrayList<>());
+        }
+        keyStore.get(key).get(timestamp).add(value);
+    }
+
+    public String get(String key, int timestamp) {
+        if (!keyStore.containsKey(key)) {
+            return "";
+        }
+        int seen = -1;
+
+        for (int time : keyStore.get(key).keySet()) {
+            if (time <= timestamp) {
+                seen = Math.max(seen, time);
+            }
+        }
+        if (seen == -1) return "";
+        int back = keyStore.get(key).get(seen).size() - 1;
+        return keyStore.get(key).get(seen).get(back);
+    }
+}
